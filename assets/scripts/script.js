27838b219f3642afad1819fbcc21abdfa396611e
ewi josepha <ewijosepha@gmail.com>
2023-08-22 17:39:58 +0200

    palindrome-checker

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,27 +1,27 @@
-// const check = document.getElementById("check");
-// const displayy = document.getElementById("display");
+const check = document.getElementById('check')
+const displayy = document.getElementById('display')
 
-// function palindrom() {
-//   let enterr = document.getElementById("start").value;
+function palindrom() {
+  let enterr = document.getElementById('start').value
 
-//   let result = "";
-//   result = enterr.split("").reverse().join("");
+  let result = ''
+  result = enterr.split('').reverse().join('')
 
-//   // console.log('this is result', result)
-//   // console.log('this is input', enterr)
+  // console.log('this is result', result)
+  // console.log('this is input', enterr)
 
-//   if (enterr === result) {
-//     displayy.innerHTML = "it is a palindrome";
-//     console.log("it is a palindrome");
+  if (enterr === result) {
+    displayy.innerHTML = 'it is a palindrome'
+    console.log('it is a palindrome')
 
-//     // return "it is a palindrome"
-//   } else {
-//     displayy.innerHTML = "it is not a palindrome";
-//     console.log("it is not a palindrome");
+    // return "it is a palindrome"
+  } else {
+    displayy.innerHTML = 'it is not a palindrome'
+    console.log('it is not a palindrome')
 
-//     // return "it is not a palindrome"
-//   }
-//   console.log(enterr);
-// }
+    // return 'it is not a palindrome'
+  }
+  console.log(enterr)
+}
 
-// check.addEventListener("click", palindrom)
+check.addEventListener('click', palindrom)
