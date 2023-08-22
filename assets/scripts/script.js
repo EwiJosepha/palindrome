9b9fc780af22b14604cc6fef0237054d3e1c997a
ewi josepha <ewijosepha@gmail.com>
2023-08-22 17:11:59 +0200

    palindrome-checker

---
+++ assets/scripts/script.js
@@ -0,0 +1,27 @@
+// const check = document.getElementById("check");
+// const displayy = document.getElementById("display");
+
+// function palindrom() {
+//   let enterr = document.getElementById("start").value;
+
+//   let result = "";
+//   result = enterr.split("").reverse().join("");
+
+//   // console.log('this is result', result)
+//   // console.log('this is input', enterr)
+
+//   if (enterr === result) {
+//     displayy.innerHTML = "it is a palindrome";
+//     console.log("it is a palindrome");
+
+//     // return "it is a palindrome"
+//   } else {
+//     displayy.innerHTML = "it is not a palindrome";
+//     console.log("it is not a palindrome");
+
+//     // return "it is not a palindrome"
+//   }
+//   console.log(enterr);
+// }
+
+// check.addEventListener("click", palindrom)
