1eacbb36a591b1f7ec62a5196f4a47362cfebe12
ewi josepha <ewijosepha@gmail.com>
2023-09-02 19:33:38 +0200

    fixing linting

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,26 +1,31 @@
 const check = document.getElementById('check')
 const displayy = document.getElementById('display')
 
-function palindrom () {
+function palindrom() {
   const enterr = document.getElementById('start').value
   let result = ''
-  const restrictSymbols = enterr.replace(/[^a-z0-9]/g, '').toLowerCase()
-  result = restrictSymbols.split('').reverse().join('')
-
-  // console.log('this is result', result)
-  // console.log('this is input', enterr)
-
-  if (restrictSymbols == result) {
-    displayy.innerHTML = 'it is a palindrome'
-    console.log('it is a palindrome')
-
-    // return "it is a palindrome"
+  if (enterr === '') {
+    displayy.innerHTML = ' type a text or number'
   } else {
-    displayy.innerHTML = 'it is not a palindrome'
-    console.log('it is not a palindrome')
+    const restrictSymbols = enterr.replace(/[^a-z0-9]/g, '').toLowerCase()
+    result = restrictSymbols.split('').reverse().join('')
 
-    // return 'it is not a palindrome'
+    // console.log('this is result', result)
+    // console.log('this is input', enterr)
+
+    if (restrictSymbols === result) {
+      displayy.innerHTML = 'it is a palindrome'
+      console.log('it is a palindrome')
+
+      // return "it is a palindrome"
+    } else {
+      displayy.innerHTML = 'it is not a palindrome'
+      console.log('it is not a palindrome')
+
+      // return 'it is not a palindrome'
+    }
   }
+
   console.log(restrictSymbols)
 }
 
